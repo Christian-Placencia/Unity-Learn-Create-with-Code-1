d4f0cc9ceb071a4768376400499044067a85b90b
Christian-Placencia <chbasaldua123@gmail.com>
2022-10-27 12:30:33 -0500

    Started Prototype 2

---
+++ Prototype 2 - Feed the Animals/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 10.0f;
+    public float xRange = 10.0f; // 10 units to the left and 10 units to the right
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal"); // GetAxis returns a value between -1 and 1.
+
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed); // Move the player right or left based on horizontalInput.
+    }
+}
